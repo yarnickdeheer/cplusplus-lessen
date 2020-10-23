dfdb1fd0e4af76d5809241d732023a30c0f9ecb3
yarnick <yarnickschool@gmail.com>
2020-10-23 21:28:30 +0200

    update

--- lesroosterApplicatie/lesroosterApplicatie/uitlezen.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-
-void Uitlezen(std::string klasnaam, int dag, int maand, int jaar)
-{
-	/// output data uit de klas clas welke lessen er overeen komen met de klasnaam die door is gegeven op de datum die is ingevoerd
-	/// 
-	/// 
-}
---
+++ opdracht1/lesroosterApplicatie/uitlezen.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+
+void Uitlezen(std::string klasnaam, int dag, int maand, int jaar)
+{
+	/// output data uit de klas clas welke lessen er overeen komen met de klasnaam die door is gegeven op de datum die is ingevoerd
+	/// 
+	/// 
+}
